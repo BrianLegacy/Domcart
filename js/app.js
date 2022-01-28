3879e2ec71f50aba75ad42b28f423e6ba5f21e09
BrianLegacy <muriuki.brian@outlook.com>
2022-01-28 16:23:05 +0300

    dom querry

---
+++ js/app.js
@@ -0,0 +1,13 @@
+let odd = document.querySelectorAll("#primary .link")
+for (let i = 0; i <= odd.length; i+=2) {
+    odd[i].style.backgroundColor = "red";
+    odd[i].style.color = "white"
+
+}
+
+
+
+
+
+
+ 
---
+++ js/tempCodeRunnerFile.js
@@ -0,0 +1,15 @@
+function chd5(num){
+//     return num % 5 == 0;
+//  }
+//  let dv5 = firstarr.filter(chd5);
+
+//  function add(bfe, aft){
+//     return bfe + aft
+//  };
+//  let sum = dv5.reduce(add);
+//  console.log(sum);
+
+
+
+
+// style.backgroundColor = "red";
